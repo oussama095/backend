a5c400886c5e84a55cb99d1c039dc9da9321f7de
oussama095 <jesuisnull95@gmail.com>
2021-05-09 13:11:32 +0200

    Initial commit

---
+++ src/main/java/com/doctor/backend/DoctorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.doctor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctor/backend/DoctorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
